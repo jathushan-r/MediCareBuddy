b93bfa40f2910e1c8c0be0ba7626037a9fbe83de
Savindu Rajapaksha <savindu.20@cse.mrt.ac.lk>
2023-08-31 11:37:19 +0530

    scheduling_appointments

--- actions/actions.py
+++ actions/actions.py
@@ -31,6 +31,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk.events import SlotSet
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from datetime import datetime, timedelta
 
 
 class ActionSayShirtSize(Action):
@@ -48,3 +49,33 @@ class ActionSayShirtSize(Action):
         else:
             dispatcher.utter_message(text=f"Your shirt size is {shirt_size}!")
         return []
+
+class ActionConfirmAppointment(Action):
+    def name(self):
+        return "action_submit_appointment_form"
+
+    def run(self, dispatcher, tracker, domain):
+
+        two_days_from_now = datetime.now() + timedelta(days=2)
+        appointment_date = two_days_from_now.strftime("%Y-%m-%d")
+        appointment_time = "15:00" 
+
+        # Fetch slot values
+        first_name = tracker.get_slot("firstname")
+        last_name = tracker.get_slot("lastname")
+        age = tracker.get_slot("age")
+        phone = tracker.get_slot("phone")
+
+        # Create the summary message
+        summary = f"Here is the information you've provided:\n"
+        summary += f"- First Name: {first_name}\n"
+        summary += f"- Last Name: {last_name}\n"
+        summary += f"- Age: {age}\n"
+        summary += f"- Phone: {phone}\n"
+        summary += f"- Appointment Date: {appointment_date}\n"
+        summary += f"- Appointment Time: {appointment_time}\n"
+        
+        summary += "\nYour appointment has been successfully booked."
+
+        # Send the summary message
+        dispatcher.utter_message(text=summary)
